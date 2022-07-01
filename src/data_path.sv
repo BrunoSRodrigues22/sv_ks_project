022b9963fca51ce164470ef8df6706bf5ef899b3
BrunoSRodrigues22 <bruno-rodrigues@uergs.edu.br>
2022-07-01 20:26:49 -0300

    Correção dos sinais do data_path

--- src/data_path.sv
+++ src/data_path.sv
@@ -6,51 +6,61 @@ import k_and_s_pkg::*;
     input  logic                    branch,
     input  logic                    pc_enable,
     input  logic                    ir_enable,
-    input  logic                    addr_sel,//ok
+    input  logic                    addr_sel,
     input  logic                    c_sel,
-    input  logic              [1:0] operation,//ok
+    input  logic              [1:0] operation,
     input  logic                    write_reg_enable,
     input  logic                    flags_reg_enable,
-    output decoded_instruction_type [4:0] decoded_instruction,
-    output logic                    zero_op,//ok
-    output logic                    neg_op,//ok
+    output decoded_instruction_type decoded_instruction,
+    output logic                    zero_op,
+    output logic                    neg_op,
     output logic                    unsigned_overflow,
     output logic                    signed_overflow,
-    output logic              [4:0] ram_addr,//ok
+    output logic              [4:0] ram_addr,
     output logic             [15:0] data_out,
     input  logic             [15:0] data_in
 
 );
     
-    //start fetch  
-    logic [15:0] instruction;
-    assign instruction = (ir_enable==1'b1)?data_in:rst_n;
-    //endfetch
-    
-    
-    //Start_decode
-        //assign a_addr = instruction[15:14];//A Addr;
-    //assign b_addr = instruction[13:12];//B Addr;
-    //assign operation = instruction[11:10];//ALU Op;
-        
-        //assign decoded_instruction = instruction[9:7];//Switch Pos;
-        //assign c_addr = instruction[6:5];//C Addr;
-        //assign mem_addr = instruction[4:0];//RW Addr;
-    //End_decode
-    
-    
-    //Operation_Config
-    logic [1:0] a_addr, b_addr, c_addr;//Address_registers
-    logic [15:0] reg_bank[3];
+    logic  [1:0] a_addr, b_addr, c_addr;//Address_registers
     logic [15:0] bus_a, bus_b, bus_c, alu_out;//Bus address
+    logic [15:0] reg_bank[3];
+    logic [15:0] instruction;
+    logic  [4:0] program_counter;
+    logic  [4:0] mem_addr;
+    logic        zero, neg, unsign, sign;//Branch_Detect
+    
+    always_ff @(posedge clk) begin : ir_ctrl
+        if(ir_enable)
+            instruction[15:0] <= data_in[15:0];
+    end : ir_ctrl
+    
+    always_comb begin : decode_comand
+        unique case(instruction[8:0])
+            'b000000000 : decoded_instruction <= I_NOP;
+            'b100000010 : decoded_instruction <= I_LOAD;
+            'b100000100 : decoded_instruction <= I_STORE;
+            'b100100010 : decoded_instruction <= I_MOVE;
+            'b101000010 : decoded_instruction <= I_ADD;
+            'b101000100 : decoded_instruction <= I_SUB;
+            'b101000110 : decoded_instruction <= I_AND;
+            'b101001000 : decoded_instruction <= I_OR;
+            'b000000010 : decoded_instruction <= I_BRANCH;
+            'b000000100 : decoded_instruction <= I_BZERO;
+            'b000010110 : decoded_instruction <= I_BNZERO;
+            'b000000110 : decoded_instruction <= I_BNEG;
+            'b000010100 : decoded_instruction <= I_BNNEG;
+            'b111111111 : decoded_instruction <= I_HALT;
+        endcase
+    end : decode_comand
   
     always_ff @(posedge clk) begin : write_reg_enable_ctrl
         if(write_reg_enable)
             reg_bank[c_addr] <= bus_c;
+            bus_a            <= reg_bank[a_addr]; 
+            bus_b            <= reg_bank[b_addr];
+            data_out         <= bus_a; 
     end : write_reg_enable_ctrl
-    
-    assign bus_a = reg_bank[a_addr]; 
-    assign bus_b = reg_bank[b_addr];
     
     always_comb begin: ula_control
         unique case(operation)
@@ -61,37 +71,38 @@ import k_and_s_pkg::*;
         endcase
     end : ula_control
     
-    assign bus_c = (c_sel==1'b1)?alu_out:data_in;//Mux_ctrl
     
-    assign data_out = bus_a;
     
-    //assign neg = alu_out[15];
+    assign    neg = (alu_out[15]==1'b1)?'b1:'b0;
     //assign zero = ~|(alu_out);
+    assign   zero = (alu_out[15:0]=='b0)?'b1:'b0;
+    assign unsign = (neg=='b0)?'b1:'b0;
+    assign   sign = (neg=='b1)?'b1:'b0;
     
     //assign zero_op = ~|(alu_out);
     //assign neg_op = alu_out[15];
     
+    always_ff @(posedge clk) begin : flag_reg
+        if(flags_reg_enable)
+            zero_op           <= zero;
+            neg_op            <= neg;
+            unsigned_overflow <= unsign;
+            signed_overflow   <= sign;
+    end : flag_reg
     
-    //Program_Counter_Config
-    logic [4:0] program_counter;
-    logic [4:0] mem_addr;
+    assign bus_c = (c_sel==1'b1)?data_in:alu_out;
     
     assign program_counter = (branch==1'b1)?mem_addr:program_counter;
     
     always_ff @(posedge clk) begin : reg_pc
         if(pc_enable)
-            program_counter = program_counter + 1;
+            if(~rst_n)
+                program_counter <= 'b0;
+            else
+                program_counter = program_counter + 1;
     end : reg_pc
     
-    assign ram_addr = (addr_sel==1'b1)?program_counter:mem_addr;
     
-    /*always_ff @(posedge clk) begin : flag_reg
-        if(flags_reg_enable)
-            //if(
-            assign neg_op = (neg==1'b1)?neg:1'b0;
-            assign zero_op = (zero==1'b1)?zero:1'b0;
-    end : flag_reg*/
-
-    //assign ram_addr = 'd0; 
+    assign ram_addr = (addr_sel==1'b1)?mem_addr:program_counter; 
 
 endmodule : data_path
